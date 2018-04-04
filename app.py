500c0fc7a4b1fb9b9c46d285fa2783b774544ab9
Solehria\alisolehria <alimuzaffar@live.com>
2018-04-04 05:22:51 +0400

    Add friend added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_socketio import SocketIO,emit
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -11,18 +11,63 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/netapps'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pass123@localhost/netapps'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
+RoomUsers = db.Table("roomUsers", db.Column("roomId",db.Integer,db.ForeignKey("room.roomID"), primary_key=True),
+db.Column("userID",db.Integer,db.ForeignKey("user.id"), primary_key=True))
+
+
+#this is friends middle table
+Friend = db.Table("friend",
+db.Column("username1",db.Integer,db.ForeignKey("user.id"),primary_key=True),
+db.Column("username2",db.Integer,db.ForeignKey("user.id"),primary_key=True))
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    firstName = db.Column(db.String(80),nullable=False)
+    lastName = db.Column(db.String(80),nullable=False)
+    DOB = db.Column(db.DateTime)
+    gender = db.Column(db.Boolean)
+    joinDate = db.Column(db.DateTime)
+    access = db.Column(db.DateTime)
+    room = db.relationship("Room", uselist=False,backref="user")
+    roomUsers = db.relationship("Room", secondary=RoomUsers)
+    message = db.relationship("Message", uselist=False,backref="user")
+    friends = db.relationship("User",secondary=Friend, primaryjoin=(Friend.c.username1 == id),secondaryjoin=(Friend.c.username2 == id))
+#    reporter = db.relationship("Report",  primaryjoin=(Report.c.reporter == id),secondaryjoin=(Report.c.reported == id), uselist=False,backref="user")
+#    reported = db.relationship("Report",primaryjoin=(Report.c.reported == id),secondaryjoin=(Report.c.reporter == id),  uselist=False,backref="user")
+
+#the room table
+class Room(db.Model):
+    roomID = db.Column(db.Integer,primary_key = True)
+    roomName = db.Column(db.String(80), unique=True, nullable=False)
+    admin = db.Column(db.Integer,db.ForeignKey("user.id"))
+    messageRoom = db.relationship("Message")
+
+#the message table
+class Message(db.Model):
+    messageID = db.Column(db.Integer,primary_key = True)
+    message = db.Column(db.String(80),nullable=False)
+    username = db.Column(db.Integer,db.ForeignKey("user.id"))
+    roomID = db.Column(db.Integer,db.ForeignKey("room.roomID"))
+    timestamp = db.Column(db.DateTime,nullable=False)
+#
+#class Report(db.Model):
+#    reoportID = db.Column(db.Integer,primary_key = True)
+#    reporter = db.Column(db.Integer,db.ForeignKey("user.id"))
+#    reported = db.Column(db.Integer,db.ForeignKey("user.id"))
+#    number = db.Column(db.Integer,nullable=False)
+#    reason = db.Column(db.String(80),nullable=False)
+#
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -34,9 +79,12 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    firstName = StringField('First Name', validators=[InputRequired(), Length(min=3, max=15)])
+    lastName = StringField('Last Name', validators=[InputRequired(), Length(min=4, max=15)])
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 
 @app.route('/')
@@ -62,10 +110,9 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
-
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password,firstName=form.firstName.data,lastName=form.lastName.data)
         db.session.add(new_user)
         db.session.commit()
 
@@ -77,7 +124,8 @@ def signup():
 @app.route('/chat')
 @login_required
 def chat():
-    return render_template('chat.html', name=current_user.username)
+
+    return render_template('chat.html', name=current_user.username, friendList = current_user.friends)
 
 @app.route('/logout')
 @login_required
@@ -85,6 +133,33 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@app.route('/getUsers', methods=['GET', 'POST'])
+@login_required
+def getUsers():
+    users = User.query.filter(User.id != current_user.id).all()
+    for user in users:
+        if user in current_user.friends:
+            users.remove(user)
+    #print(users)
+    return render_template("usersList.html",users = users)
+
+
+
+@app.route('/addFriend', methods=['GET', 'POST'])
+@login_required
+def addFriend():
+    userid = request.form['friends']
+    print(userid)
+    new_friend = User.query.filter_by(id = userid).first()
+    print(current_user.friends)
+    current_user.friends.append(new_friend)
+    db.session.commit()
+    return ""
+
+
 if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     socketio.run(app, debug=True)
-    app.run(debug=True)
+    #app.run(debug=True)
--- model.py
+++ model.py
@@ -2,21 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/netapps'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pass123@localhost/netapps'
 db = SQLAlchemy(app)
 
 #this is the middle table for users and rooms
 
-roomUsers = db.Table("roomUsers", db.Column("roomId",db.Integer,db.ForeignKey("room.roomID"), primary_key=True),
+
+
+RoomUsers = db.Table("roomUsers", db.Column("roomId",db.Integer,db.ForeignKey("room.roomID"), primary_key=True),
 db.Column("userID",db.Integer,db.ForeignKey("user.id"), primary_key=True))
 
 
 #this is friends middle table
-friend = db.Table("friend",
+Friend = db.Table("friend",
 db.Column("username1",db.Integer,db.ForeignKey("user.id"),primary_key=True),
 db.Column("username2",db.Integer,db.ForeignKey("user.id"),primary_key=True))
 
-#this is the user table
 class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -24,38 +25,38 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     firstName = db.Column(db.String(80),nullable=False)
     lastName = db.Column(db.String(80),nullable=False)
-    DOB = db.Column(db.DateTime,nullable=False)
+    DOB = db.Column(db.DateTime)
     gender = db.Column(db.Boolean)
-    joinDate = db.Column(db.DateTime,nullable=False)
-    access = db.Column(db.DateTime,nullable=False)
-    room = db.relationship("room", uselist=False,backref="user")
-    roomUsers = db.relationship("RoomUsers", secondary=roomUsers, lazy="subquery",
-        backref=db.backref("user", lazy=True))
-    message = db.relationship("message", uselist=False,backref="user")
-    friends = db.relationship("user", secondary=friend,lazy="subquery",
-        backref=db.backref("user", lazy=True))
-    reporter = db.relationship("report", uselist=False,backref="user")
-    reported = db.relationship("report", uselist=False,backref="user")
+    joinDate = db.Column(db.DateTime)
+    access = db.Column(db.DateTime)
+    room = db.relationship("Room", uselist=False,backref="user")
+    roomUsers = db.relationship("Room", secondary=RoomUsers)
+    message = db.relationship("Message", uselist=False,backref="user")
+    friends = db.relationship("User",secondary=Friend, primaryjoin=(Friend.c.username1 == id),secondaryjoin=(Friend.c.username2 == id))
+    reporter = db.relationship("Report", uselist=False,backref="user")
+    reported = db.relationship("Report", uselist=False,backref="user")
+
 #the room table
 class Room(db.Model):
     roomID = db.Column(db.Integer,primary_key = True)
     roomName = db.Column(db.String(80), unique=True, nullable=False)
     admin = db.Column(db.Integer,db.ForeignKey("user.id"))
-    messageRoom = db.relationship("message", uselist=False,backref="user")
+    messageRoom = db.relationship("Message", uselist=False,backref="user")
 
 #the message table
-class message(db.Model):
+class Message(db.Model):
     messageID = db.Column(db.Integer,primary_key = True)
     message = db.Column(db.String(80),nullable=False)
     username = db.Column(db.Integer,db.ForeignKey("user.id"))
     roomID = db.Column(db.Integer,db.ForeignKey("room.roomID"))
     timestamp = db.Column(db.DateTime,nullable=False)
 
-class report(db.Model):
+class Report(db.Model):
     reoportID = db.Column(db.Integer,primary_key = True)
     reporter = db.Column(db.Integer,db.ForeignKey("user.id"))
     reported = db.Column(db.Integer,db.ForeignKey("user.id"))
     number = db.Column(db.Integer,nullable=False)
     reason = db.Column(db.String(80),nullable=False)
+
 
 db.create_all()
